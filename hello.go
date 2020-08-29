38dfe550ca4fe064fb300b3d924388af60d29fdc
Evelyn G. Salas Ramos <evelyn.salas888@gmail.com>
2020-08-28 19:20:11 -0700

    notes on variables

--- hello.go
+++ hello.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	//basic print command
 	fmt.Printf("hello, world!!\n")
+
+	//declaring variables, method 1
+	//good for initializing later
+	//can be used to declare variables
+	//outside of the package
+	var i int
+	i = 42
+	i = 27
+	fmt.Println(i)
+
+	//declaring variables, method 2
+	//good if go gets the type wrong
+	var j int = 15
+	fmt.Println(j)
+
+	//decalring variables, method 3
+	k := 14.
+	fmt.Println(k)
+
+	//how to check the variable type
+	fmt.Printf("%v, %T", k, k)
+
+	//type conversions
+	var m float32
+	m = float32(i)
+	fmt.Printf("%v, %T", m, m)
 }
